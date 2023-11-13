400ee3fe0a371a15a1b64c8e40fc8d8a8918eb20
paulecode <paul.ebert@code.berlin>
2023-11-13 21:56:21 +0100

    adjusted app.ts for new logger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 const express = require('express')
+const logger = require('../utils/logger')
+
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
     res.send('Hello World!')
 })
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
+    logger.info('listening on port 3000!')
 })
