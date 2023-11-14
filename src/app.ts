8b2716429b3dbe1f58260d962127d657c6da0de7
paulecode <paul.ebert@code.berlin>
2023-11-14 19:01:50 +0100

    implemented category router

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express'
 import logger from '../utils/logger'
+import categoryRoutes from './routes/categoryRoutes'
 
 const app = express()
+const router = express.Router()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+router.use('/categories', categoryRoutes)
+
+app.use('/api', router)
 
 app.listen(PORT, () => {
     logger.info(`listening on port ${PORT}!`)
